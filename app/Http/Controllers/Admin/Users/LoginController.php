91ef87a72b5276521f419a271e7713a9cc27941b
TheAnh <tozaco1@gmail.com>
2024-05-10 02:04:29 +0700

    feat:login

---
+++ app/Http/Controllers/Admin/Users/LoginController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Users;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    public function index(){
+       return view('admin.users.login',[
+        'title' => 'Login'
+       ]);
+    }
+
+    public function store(Request $request){
+        $this->validate($request,[
+            'email' => 'required|email:filter',
+            'password' => 'required'
+        ]);
+    }
+}
