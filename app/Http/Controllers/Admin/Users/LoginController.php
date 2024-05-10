4f1bb2fd9dbc04ef2d60a318b9618616a4b4726e
TheAnh <tozaco1@gmail.com>
2024-05-10 23:38:07 +0700

    fix:redirect

--- app/Http/Controllers/Admin/Users/LoginController.php
+++ app/Http/Controllers/Admin/Users/LoginController.php
@@ -4,19 +4,36 @@ namespace App\Http\Controllers\Admin\Users;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
     public function index(){
-       return view('admin.users.login',[
+    return view('admin.users.login',[
         'title' => 'Login'
-       ]);
+    ]);
     }
 
     public function store(Request $request){
+        // dd($request->input());
         $this->validate($request,[
             'email' => 'required|email:filter',
             'password' => 'required'
         ]);
+
+        // $credentials = $request->only('email', 'password');
+        // if (Auth::attempt($credentials)) {
+        //     // Authentication passed...
+        //     return redirect()->intended('dashboard');
+        // }
+
+        if (Auth::attempt([
+            'email' => $request->input("email"),
+            'password' => $request->input('password')
+        ],
+        $request->input('remember'))){
+            return redirect()->route('admin');
+        }
+        return redirect()->back();
     }
 }
