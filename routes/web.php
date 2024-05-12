14f8b94266450903d8f44cc172e09d45739fa571
TheAnh <tozaco1@gmail.com>
2024-05-12 09:04:19 +0700

    feat: Setup Admin UI

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,23 @@
 <?php
 
 use App\Http\Controllers\Admin\MainController;
+use App\Http\Controllers\Admin\MenuController;
 use App\Http\Controllers\Admin\Users\LoginController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('admin/users/login',[LoginController::class,'index']);
+Route::get('admin/users/login',[LoginController::class,'index'])->name('login');
 Route::post('admin/users/login/store',[LoginController::class,'store']);
-Route::get('admin/main', [MainController::class,'index'])->name('admin');
+
+Route::middleware(['auth'])->group(function(){
+
+    Route::prefix('admin')->group(function () {
+
+        Route::get('/', [MainController::class,'index'])->name('admin');
+        Route::get('main', [MainController::class,'index']);
+
+        #Menu
+        Route::prefix('menus')->group(function(){
+            Route::get('add',[MenuController::class,'create']);
+        });
+    });
+});
